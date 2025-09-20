8b19824f20b0678520faf0b3335ae6f18fdb5faf
Shrylle Ritchnicholas D. Ho <shrylleritchnicholas.ho@cit.edu>
2025-09-20 13:10:14 +0800

    implement function

--- src/boardarray.h
+++ src/boardarray.h
@@ -12,11 +12,38 @@ class BoardArray : public Board {
             array = new Entry[SIZE];
             index = 0;
         }
-
-        void add(Entry* entry) {
-            // TODO: IMPLEMENT THIS FUNCTION
-            // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+ 
+            
+    void add(Entry* entry) {
+        // TODO: IMPLEMENT THIS FUNCTION
+        // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+        
+        if (index >= SIZE && !entry->compare(&array[SIZE - 1])) {
+            std::cout << entry->name << "'s score is too low to be added!" << std::endl;
+            return;
+    }
+    
+    int pos = index;
+        for (int i = 0; i < index; i++) {
+            if (entry->compare(&array[i])) {
+                pos = i;
+            break;
         }
+    }
+    
+    if (index < SIZE) {
+        for (int i = index; i > pos; i--) {
+            array[i] = array[i - 1];
+        }
+        index++;
+    } else {
+        for (int i = SIZE - 1; i > pos; i--) {
+            array[i] = array[i - 1];
+        }
+    }
+    
+    array[pos] = *entry;
+}
 
         void print() {
             for (int i = 0; i < index; i++) {
